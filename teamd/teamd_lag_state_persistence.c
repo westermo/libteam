95b8ce1b75a43e596b52f4bfb12802d566da8dfb
Jacques de Laval <Jacques.De.Laval@westermo.com>
2019-09-17 08:56:12 +0200

    teamd-ttdp: Document state files

--- teamd/teamd_lag_state_persistence.c
+++ teamd/teamd_lag_state_persistence.c
@@ -81,6 +81,13 @@ FILE *open_lag_state_file(const char *team_devname, const char *path, const char
 	return fopen(pathname, mode);
 }
 
+/**
+ * lag_state_write_identity - Persist aggregate identity to state files
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/identity/mac - Hardware address (MAC) as string
+ * /run/train/lag/<DEVICE-NAME>/identity/uuid - UUID of local consist as string
+ */
 int lag_state_write_identity(struct teamd_context *ctx, void *priv) {
 	struct ab *ab = priv;
 	if (!(ab->identity_hwaddr_set) || !(ab->local_uuid_set)) {
@@ -121,6 +128,16 @@ int lag_state_write_identity(struct teamd_context *ctx, void *priv) {
 	return 0;
 }
 
+/**
+ * lag_state_write_line_status - Persist aggregate line statuses to state files
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/line/direction - Direction of aggregated lines, possible values: "0" or "1"
+ * /run/train/lag/<DEVICE-NAME>/line/<LINE>/status - Status of line, possible values: "0" (Error), "1" (False), "2" (True) or "3" (Undefined)
+ * /run/train/lag/<DEVICE-NAME>/line/<LINE>/connected_neighbor - Connected neighbor line, possible values: "A", "B", "C", "D" or "-" (Undefined)
+ * /run/train/lag/<DEVICE-NAME>/line/<LINE>/port_state - Port state, possible values: "0" (Disabled), "2" (Forwarding) or "3" (Discarding)
+ * /run/train/lag/<DEVICE-NAME>/line/_commit - A dummy value is written to this file to indicate that aggregate line statuses have changed
+ */
 int lag_state_write_line_status(struct teamd_context *ctx, void *priv) {
 	struct ab *ab = priv;
 	FILE *state_fp = NULL;
@@ -187,6 +204,12 @@ int lag_state_write_line_status(struct teamd_context *ctx, void *priv) {
 	return 0;
 }
 
+/**
+ * lag_state_write_aggregate_role - Persist aggregate role to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/aggregate_role - Aggregate role, possible values: "0" (Floating end), "1" (Floating intermediate), "2" (Fixed end) or "3" (Fixed intermediate)
+ */
 int lag_state_write_aggregate_role(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "aggregate_role", "w");
 	if (!fp) {
@@ -197,6 +220,12 @@ int lag_state_write_aggregate_role(struct teamd_context *ctx, struct ab *ab) {
 	return 0;
 }
 
+/**
+ * lag_state_write_diag_crossed_lines_detected - Persist aggregate crossed lines status to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/diag/crossed_lines - Crossed lines status, possible values: "0" (False) or "1" (True)
+ */
 int lag_state_write_diag_crossed_lines_detected(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "diag/crossed_lines", "w");
 	if (!fp) {
@@ -207,6 +236,12 @@ int lag_state_write_diag_crossed_lines_detected(struct teamd_context *ctx, struc
 	return 0;
 }
 
+/**
+ * lag_state_write_diag_mixed_consist_orientation_detected - Persist aggregate mixed consist orientation status to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/diag/mixed_consist_orientation - Mixed consist orientation status, possible values: "0" (False) or "1" (True)
+ */
 int lag_state_write_diag_mixed_consist_orientation_detected(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "diag/mixed_consist_orientation", "w");
 	if (!fp) {
@@ -217,6 +252,12 @@ int lag_state_write_diag_mixed_consist_orientation_detected(struct teamd_context
 	return 0;
 }
 
+/**
+ * lag_state_write_remote_inhibition - Persist aggregate remote inhibition status to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/remote_inhibition - Remove inhibition, possible values: "1" (False), "2" (True) or "3" (Undefined)
+ */
 int lag_state_write_remote_inhibition(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "remote_inhibition", "w");
 	if (!fp) {
@@ -227,6 +268,13 @@ int lag_state_write_remote_inhibition(struct teamd_context *ctx, struct ab *ab) 
 	return 0;
 }
 
+/**
+ * lag_state_write_hello_timeouts - Persist aggregate hello timeout values to state files
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/ttdp_hello_timeout/slow - Slow TTDP HELLO timeout as integer
+ * /run/train/lag/<DEVICE-NAME>/ttdp_hello_timeout/fast - Fast TTDP HELLO timeout as integer
+ */
 int lag_state_write_hello_timeouts(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "ttdp_hello_timeout/slow", "w");
 	if (!fp) {
@@ -245,6 +293,12 @@ int lag_state_write_hello_timeouts(struct teamd_context *ctx, struct ab *ab) {
 	return 0;
 }
 
+/**
+ * lag_state_write_elected_neighbor - Persist aggregate elected neighbor to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/elected_neighbor/mac - Elected neighbor hardware address (MAC) as string
+ */
 int lag_state_write_elected_neighbor(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "elected_neighbor/mac", "w");
 	if (!fp) {
@@ -261,6 +315,12 @@ int lag_state_write_elected_neighbor(struct teamd_context *ctx, struct ab *ab) {
 	return 0;
 }
 
+/**
+ * lag_state_write_shortening_detected - Persist aggregate shortening status to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/shortening_detected - Shortening detected, possible values "0" (False) or "1" (True)
+ */
 int lag_state_write_shortening_detected(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "shortening_detected", "w");
 	if (!fp) {
@@ -271,6 +331,12 @@ int lag_state_write_shortening_detected(struct teamd_context *ctx, struct ab *ab
 	return 0;
 }
 
+/**
+ * lag_state_write_lengthening_detected - Persist aggregate lengthening status to state file
+ *
+ * State files:
+ * /run/train/lag/<DEVICE-NAME>/lengthening_detected - Lengthening detected, possible values "0" (False) or "1" (True)
+ */
 int lag_state_write_lengthening_detected(struct teamd_context *ctx, struct ab *ab) {
 	FILE *fp = open_lag_state_file(ctx->team_devname, "lengthening_detected", "w");
 	if (!fp) {
